bf4211996a4daf9dc8b61f28821d02d7222f18d0
kshitijwohlig <kshitij.gaikwad@wohlig.com>
2024-01-22 17:21:14 +0530

    changes

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/devops', function (req, res) {
   res.send('This is devops')
 })
  
-app.listen(3400,()=>{
-    console.log(`Connected to the port 3400`);
+app.listen(8080,()=>{
+    console.log(`Connected to the port 8080`);
 })  
+// app.listen(3400,()=>{
+//     console.log(`Connected to the port 3400`);
+// })  
